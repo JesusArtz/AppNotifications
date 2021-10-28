03dc3c65f476a6374d5c4a537b0c8f079385d655
JesusArtz <85129357+JesusArtz@users.noreply.github.com>
2021-10-27 20:22:59 -0500

    Added More Notifications

--- awita.py
+++ awita.py
@@ -14,6 +14,20 @@ if platform == "win32":
 elif platform == "linux" or platform == "linux2":
     import notify2
 
+root = Tk()
+estilo = Style(theme='pers')
+ventana = estilo.master
+root.title("App")
+root.geometry("600x450")
+imagen = PhotoImage(file= "Assets/CTRL.png")
+Label(root, image= imagen).pack(pady=5)
+
+#Variables de Checkbutton
+
+motivacion = IntVar()
+salud = IntVar()
+datosCuriosos = IntVar()
+
 url = "https://www.buymeacoffee.com/jesusartz"
 
 def BuyMeaCoffee():
@@ -30,33 +44,41 @@ def WindowsMessage(tittle, description):
     return
 
 def mainProgram():
-    if platform == "linux" or platform == "linux2":
-        LinuxMessage("Recuerda beber agua", "Cuida tu cuerpo y mantente centrado <3")
-        time.sleep(1000)
-            
+    if (motivacion.get()==1) and (salud.get()==0) and (datosCuriosos==1):
 
-
-    elif platform == "win32":
-        WindowsMessage("Recuerda beber agua", "Cuida tu cuerpo y mantente centrado <3")
-        time.sleep(100)
+        if platform == "linux" or platform == "linux2":
+            LinuxMessage("¿Los sabias?", 'Más del 70% de todos los trabajos de programación son en campos e industrias fuera de la tecnología.')
+            time.sleep(1000)
+            LinuxMessage("¿Lo sabias?", "El primer lenguaje de programación del mundo se llamó FORTRAN")
+            time.sleep(1000)
+            LinuxMessage("¿Lo sabias?", "Hoy en día, ¡hay más de 700 lenguajes de programación en el mundo!")
+            time.sleep(1000)
+            LinuxMessage("¿Lo sabias?", "Triller de Michael Jackson es el album mas vendido de la historia")
+            time.sleep(1000)
+            LinuxMessage("Cuidadoooo", "Usa siempre Console Log")
+            time.sleep(1000)
+        
+        elif platform == "win32":
+            WindowsMessage("¿Los sabias?", 'Más del 70% de todos los trabajos de programación son en campos e industrias fuera de la tecnología.')
+            time.sleep(1000)
+            WindowsMessage("¿Lo sabias?", "El primer lenguaje de programación del mundo se llamó FORTRAN")
+            time.sleep(1000)
+            WindowsMessage("¿Lo sabias?", "Hoy en día, ¡hay más de 700 lenguajes de programación en el mundo!")
+            time.sleep(1000)
+            WindowsMessage("¿Lo sabias?", "Triller de Michael Jackson es el album mas vendido de la historia")
+            time.sleep(1000)
+            WindowsMessage("Cuidadoooo", "Usa siempre Console Log")
+            time.sleep(1000)
+            WindowsMessage("Recuerda beber agua", "Cuida tu cuerpo y mantente centrado <3")
+            time.sleep(1000)
         
 def bucle():
     while True:
         mainProgram()
 
-root = Tk()
-estilo = Style(theme='pers')
-ventana = estilo.master
-root.title("App")
-root.geometry("600x450")
-imagen = PhotoImage(file= "Assets/CTRL.png")
-Label(root, image= imagen).pack(pady=5)
 
-#Variables de Checkbutton
 
-motivacion = IntVar()
-salud = IntVar()
-datosCuriosos = IntVar()
+
 
 #Botones y Checkbuttons
 
@@ -66,7 +88,7 @@ botonCoffee = Button(text="Buy Me a Coffee", command=BuyMeaCoffee)
 botonCoffee.place(x= 450, y= 400)
 botonStop.place(x=300, y=400)
 botonInicio.place(x=150, y=400)
-check1 = Checkbutton(text="Curiosidades", variable=datosCuriosos, onvalue=1, offvalue=0)
+check1 = Checkbutton(text="Curiosidades y Consejos", variable=datosCuriosos, onvalue=1, offvalue=0)
 check1.place(x=75, y=250)
 check2 = Checkbutton(text="Salud", variable=salud, onvalue=1, offvalue=0)
 check2.place(x=75, y=300)
