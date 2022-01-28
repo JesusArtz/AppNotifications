bbdc14a9f320442bd35047cb2133841c2f2a1430
Jesus <85129357+JesusArtz@users.noreply.github.com>
2022-01-27 22:18:00 -0600

    API has been updated

--- app.py
+++ app.py
@@ -7,16 +7,17 @@ from tkinter import ttk
 from ttkbootstrap import Style
 import webbrowser
 from sys import platform
-#Importa win10toast si el sistema operativo es Windows
+
+# Importa win10toast si el sistema operativo es Windows
 if platform == "win32":
     from win10toast import ToastNotifier
+
     notificacion = ToastNotifier()
-#Importa notify2 si el sistema operativo es Linux
+# Importa notify2 si el sistema operativo es Linux
 elif platform == "linux" or platform == "linux2":
     import notify2
 
 stop_threads = False
-
 
 # Carga la ventana de Tkinter
 
@@ -25,14 +26,11 @@ estilo = Style(theme='alt')
 ventana = estilo.master
 root.title("App")
 root.geometry("600x450")
-imagen = PhotoImage(file= "Assets/CTRL.png")
-Label(root, image= imagen).pack(pady=5)
-
+imagen = PhotoImage(file="Assets/CTRL.png")
+Label(root, image=imagen).pack(pady=5)
 
 varDatos = IntVar()
 varMotivacion = IntVar()
-
-
 
 # Define la listas donde se almacenaran los datos de la API
 listaTitulos = []
@@ -44,17 +42,17 @@ listaMD = []
 listaTM = []
 listaMM = []
 
-headers = headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+headers = headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 response = requests.get("http://137.184.124.114:8080/api/datos", headers=headers)
 
 api = response.json()
 
-
-for i in api['postData']:
+for i in api:
     listaTD.append(i['TituloD'])
     listaMD.append(i['MensajeD'])
 
-for i in api['postData']:
+for i in api:
     listaTM.append(i['TituloS'])
     listaMM.append(i['MensajeS'])
 
@@ -64,16 +62,16 @@ listaTitulos.append(listaTM)
 listaMensajes.append(listaMD)
 listaMensajes.append(listaMM)
 
-
 # Detecta el tamaño de las listas
-
-
 
 
 # Hace Funcionar el boton de BuyMeaCoffe (PARTE ESENCIAL NO TOCAR)
 url = "https://www.buymeacoffee.com/jesusartz"
+
+
 def BuyMeaCoffee():
     webbrowser.open_new(url)
+
 
 # Define los parametros de notificacion para Linux
 def LinuxMessage(title, description):
@@ -82,10 +80,12 @@ def LinuxMessage(title, description):
     notify.show()
     return
 
+
 # Define los parametros de notificacion para Windows
 def WindowsMessage(tittle, description):
     notificacion.show_toast(tittle, description, duration=5)
     return
+
 
 # Correr programa principal
 def mainProgram():
@@ -104,13 +104,13 @@ def mainProgram():
             choise = random.choice(choiseL)
             WindowsMessage(listaTitulos[choise][randomNum], listaMensajes[choise][randomNum])
             time.sleep(15)
-    
-        
+
+
 
     elif (varDatos.get() == 0) & (varMotivacion.get() == 1):
         if platform == "linux" or platform == "linux2":
             randomNum = random.randint(0, 50)
-            
+
             LinuxMessage(listaTitulos[0][randomNum], listaMensajes[0][randomNum])
             time.sleep(15)
 
@@ -118,7 +118,7 @@ def mainProgram():
             randomNum = random.randint(0, 50)
             WindowsMessage(listaTitulos[0][randomNum], listaMensajes[0][randomNum])
             time.sleep(15)
-        
+
 
     elif (varDatos.get() == 1) & (varMotivacion.get() == 0):
 
@@ -131,9 +131,7 @@ def mainProgram():
             randomNum = random.randint(51, 76)
             WindowsMessage(listaTitulos[1][randomNum], listaMensajes[1][randomNum])
             time.sleep(15)
-        
-    
-    
+
 
 # Esta funcion crea un hilo en el sistema para poder usar la interfaz a la vez que el bucle se ejecuta
 def bucle():
@@ -142,34 +140,32 @@ def bucle():
     while True:
 
         mainProgram()
-        
+
         if stop_threads == True:
             break
         # Vale verga la eficiencia
-    
+
+
 def Stop():
     global stop_threads
     stop_threads = True
 
-#Botones y Checkbuttons
 
-botonInicio = Button(text="Start", command=lambda:threading.Thread(target=bucle).start())
+# Botones y Checkbuttons
+
+botonInicio = Button(text="Start", command=lambda: threading.Thread(target=bucle).start())
 
 c2 = Checkbutton(root, text="Motivacion", variable=varDatos, onvalue=1, offvalue=0)
 c3 = Checkbutton(root, text="Datos Curioso", variable=varMotivacion, onvalue=1, offvalue=0)
 
-c2.place(x= 100, y=300)
-c3.place(x= 100, y=275)
+c2.place(x=100, y=300)
+c3.place(x=100, y=275)
 botonStop = Button(text="Stop", command=Stop)
 botonCoffee = Button(text="Buy Me a Coffee", command=BuyMeaCoffee)
-botonCoffee.place(x= 450, y= 400)
+botonCoffee.place(x=450, y=400)
 botonStop.place(x=300, y=400)
 botonInicio.place(x=150, y=400)
 
-#Main Loop
+# Main Loop
 
 root.mainloop()
-
-
-# Coded with love for Emma
-
